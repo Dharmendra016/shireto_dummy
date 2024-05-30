3ce09f0a4b235314e55812af7385b59b32271db1
dharmendra016 <dharmendra.078bct016@tcioe.edu.np>
2024-05-31 01:31:22 +0545

    home page implemented

--- script.js
+++ script.js
@@ -53,3 +53,20 @@ document.addEventListener('mousemove', (e) => {
         ease: 'power2.out'
     });
 });
+
+//nav animation 
+gsap.from(".nlink" , {
+    stagger:.2,
+    y:10 , 
+    duration:2, 
+    ease: Power3 , 
+    opacity:0
+})
+
+//image animation 
+
+Shery.imageEffect(".home-page" , {
+    style:5,
+    config:{"a":{"value":1.6,"range":[0,30]},"b":{"value":-0.92,"range":[-1,1]},"zindex":{"value":"100","range":[-9999999,9999999]},"aspect":{"value":2.0850077279752703},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":2.5,"range":[1,15]},"durationOut":{"value":0.44,"range":[0.1,5]},"durationIn":{"value":0.47,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":false},"onMouse":{"value":1},"noise_speed":{"value":0.46,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2],"_gsap":{"id":11}},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.46,"range":[0,2]},"noise_scale":{"value":17.56,"range":[0,100]}},
+    gooey:true,
+})
