0c1cb4f47964523ec007eef3e3f12a55379e0a79
dharmendra016 <dharmendra.078bct016@tcioe.edu.np>
2024-06-02 18:34:58 +0545

    bug fixing

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-Hello 
+
+# Shireto_Dummy
+This a dummy website build on HTML , CSS , JS . 
+Used different libraries such as gsap.js ,locomotive.js,scrolltrigger.js , shery.js
+
+Original website 
+https://teamshireto.com/
+
+
+## Demo
+https://github.com/Dharmendra016/shireto_dummy/assets/107425784/949e729b-5e3e-4cdb-b103-bd092661973d
+
+
+## How to run the project
+
+```bash
+  1.fork/clone this repo.
+    https://github.com/Dharmendra016/shireto_dummy.git
+  2.open in vscode 
+  3.open with live server .
+```
+    
+
+
+
+
--- script.js
+++ script.js
@@ -47,9 +47,9 @@ gsap.from(".nlink" , {
 
 //image animation 
 
-Shery.imageEffect(".team-images" , {
-    style:5,
-    config:{"a":{"value":1.6,"range":[0,30]},"b":{"value":-0.92,"range":[-1,1]},"zindex":{"value":"100","range":[-9999999,9999999]},"aspect":{"value":2.0850077279752703},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":2.5,"range":[1,15]},"durationOut":{"value":0.44,"range":[0.1,5]},"durationIn":{"value":0.47,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":false},"onMouse":{"value":1},"noise_speed":{"value":0.46,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2],"_gsap":{"id":11}},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.46,"range":[0,2]},"noise_scale":{"value":17.56,"range":[0,100]}},
+Shery.imageEffect("#team-desc .team-images" , {
+    style:4,
+    config:{"a":{"value":2.75,"range":[0,30]},"b":{"value":-0.94,"range":[-1,1]},"zindex":{"value":"10","range":[-9999999,9999999]},"aspect":{"value":1.25099014683153},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":2.5,"range":[1,15]},"durationOut":{"value":0.44,"range":[0.1,5]},"durationIn":{"value":0.47,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.27,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":false},"onMouse":{"value":1},"noise_speed":{"value":0.61,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2],"_gsap":{"id":11}},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.46,"range":[0,2]},"noise_scale":{"value":17.56,"range":[0,100]}},
     gooey:true, 
 })
 
@@ -57,7 +57,6 @@ Shery.imageEffect(".team-images" , {
 
 Shery.imageEffect(".formula-bharat-page .right-part" , {
     style:3 , 
-    // debug:true,
 })
 
 
